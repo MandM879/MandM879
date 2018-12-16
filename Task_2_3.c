4ecb8e551f3016ffce65ac2a73d6538d6e9191fc
RsMslk <44443558+RsMslk@users.noreply.github.com>
2018-12-16 22:40:01 +0300

    Updated CHECK - READY TASK2_3.C
    ADDED REVERSE FIXED ALL FUNCS

--- Task_2_3.c
+++ Task_2_3.c
@@ -4,7 +4,7 @@
 //struct of list
 typedef struct _List
 {
-    void* value;
+    int * value;
     struct _List *next;
     struct _List *prev;
 } List;
@@ -40,7 +40,7 @@ int list_delete(Double_List **l) {
 }
 
 //push
-void push (Double_List *l, void* a){
+void push (Double_List *l, void* a) {
 	List *tmp = (List*) malloc(sizeof(List));
 	if (tmp == NULL) {
     	exit(1);
@@ -49,40 +49,48 @@ void push (Double_List *l, void* a){
 	tmp->next = NULL;
 	tmp->prev = l->tail;
 
-	if(l->tail != NULL){
+	if(l->tail != NULL) {
 		l->tail->next = tmp;
 	}
 	l->tail = tmp;
-	if(l->head == NULL){
+	if(l->head == NULL) {
 		l->head = tmp;
 	}
 	l->size ++;
 	//return 0;
 }
-//pop
-void* pop(Double_List *l) {
-    List *next;
-    void *tmp;
-    if (l->tail == NULL) {
-        exit(1);
-    }
- 
-    next = l->tail;
-    l->tail = l->tail->prev;
-    if (l->tail) {
-        l->tail->next = NULL;
-    }
-    if (next == l->head) {
-        l->head = NULL;
-    }
-    tmp = next ->value;
-    free(next);
- 
-    l->size --;
-    return tmp;
+
+//pop without argument
+int* pop_no_arg(Double_List *l) {
+	List *next;
+	void *tmp;
+	if (l->tail == NULL) {
+		exit(1);
+	}
+
+	next = l->tail;
+	l->tail = l->tail->prev;
+	if (l->tail) {
+		l->tail->next = NULL;
+	}
+	if (next == l->head) {
+		l->head = NULL;
+	}
+	tmp = next->value;
+	free(next);
+
+	l->size--;
+	return tmp;
 }
+
+//true pop
+void pop(Double_List *l, int *a) {
+	*a = *(l->tail->value);
+	pop_no_arg(l);
+}
+
 //unshift
-void unshift (Double_List *l, void * a){
+void unshift (Double_List *l, void * a) {
 	List *tmp = (List*) malloc(sizeof(List));
 	if (tmp == NULL) {
     	exit(1);
@@ -100,33 +108,58 @@ void unshift (Double_List *l, void * a){
 	}
 	l->size ++;
 }
-//shift
-void *shift (Double_List *l){
+//shift without taking arg
+void* shift_no_arg (Double_List *l) {
 	List *prev;
 	void *tmp;
+	//*a = *(l->head->value);
 	if (l->head == NULL) {
 		exit(1);
 	}
 
 	prev = l->head;
 	l->head = l->head->next;
-	if(l->head){
+	if(l->head) {
 		l->head->prev = NULL;
 	}
-	if(prev == l->tail){
+	if(prev == l->tail) {
 		l->tail = NULL;
 	}
-	tmp =prev-> value;
+	tmp = prev->value;
 	free(prev);
 	l->size --;
+	
 	return tmp;
 }
 
+//true shift
+void shift(Double_List *l, int* a) {
+	*a = *(l->head->value);
+	shift_no_arg(l);
+}
+
 //reverse
+void reverse(Double_List *l) { 
+	List *tmp1 = l->head->next;
+	List *tmp2 = l->head->value;
+	l->head->next = NULL;
+	l->head->prev = tmp1;
+	tmp1 = l->head->prev;
+	while (tmp1->next) {
+		tmp2 = tmp1->next;
+		tmp1->next = tmp1->prev;
+		tmp1->prev = tmp2;
+		tmp1 = tmp1->prev;
+	}
+	tmp1 = l->tail->prev;
+	l->tail->prev = NULL;
+	l->tail->next = tmp1;
+	tmp1 = l->head;
+	l->head = l->tail;
+	l->tail = tmp1;
+}
 
-// void reverse
-
-
+//print
 void print(Double_List *l, void (*fun)(void*)) {
     List *tmp = l->head;
     while (tmp) {
@@ -135,7 +168,6 @@ void print(Double_List *l, void (*fun)(void*)) {
     }
     printf("\n");
 }
-
 void printInt(void *value) {
     printf("%d ", *((int*) value));
 }
@@ -143,16 +175,18 @@ void printInt(void *value) {
 
 int main() {
     Double_List *list = list_new();
-    int a, b, c, d, e, f, g, h;
-     
-    a = 10;
-    b = 20;
-    c = 30;
-    d = 40;
-    e = 50;
-    f = 60;
-    g = 70;
-    h = 80;
+    int a, b, c, d, e, f, g, h,j;
+    int *i;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    e = 5;
+    f = 6;
+    g = 7;
+    h = 8;
+    j = 0;
+    i = &j;
     unshift(list, &a);
     unshift(list, &b);
     unshift(list, &c);
@@ -160,23 +194,16 @@ int main() {
     push(list, &e);
     push(list, &f);
     print(list, printInt);
-    printf("length %d\n", list->size);
-    printf("shift %d\n", *((int*)shift(list)));
-    printf("shift %d\n", *((int*)shift(list)));
-    printf("head %d\n", *((int*)(list->head->value)));
-    printf("tail %d\n", *((int*)(list->tail->value)));
-    printf("pop %d\n", *((int*)pop(list)));
-    printf("pop %d\n", *((int*)pop(list)));
-    printf("length %d\n", list->size);
-     
-    //getch();
+
+    pop(list, i);
+	printf("%d\n", *i);
+	print(list, printInt);
+	shift(list, i);
+	printf("%d\n", *i);
+	print(list, printInt);
+	reverse(list);
+	print(list, printInt);
+
+
     return 0;
 }
-
-
-
-
-
-
-
-
